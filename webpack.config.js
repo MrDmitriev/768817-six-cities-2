e74a80466107eee58a182bf738c26ab02894a580
dmitriev_sergey <info@davra.cz>
2019-12-21 19:30:49 +0100

    add npm deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require(`path`);
 
 module.exports = {
   entry: `./src/index.js`,
+  performance: {
+    hints: false
+  },
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
